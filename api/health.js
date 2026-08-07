b059448fafd625d4150acaccb55ebb15dd7d8512
terrasayangini <ircham.ferdiansyah@gmail.com>
2026-08-07 18:20:56 +0700

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,5 +1,10 @@
 export default function handler(req, res) {
+
+  const groq = process.env.GROQ_API_KEY;
+
   res.status(200).json({
-    status: "online"
+    status: "online",
+    groq: groq ? "CONNECTED ✅" : "NOT FOUND ❌"
   });
+
 }
