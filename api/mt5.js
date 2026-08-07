be90177a7490d7c67b820a45a250e64420473bfa
terrasayangini <ircham.ferdiansyah@gmail.com>
2026-08-07 11:31:11 +0700

    Update mt5.js

--- api/mt5.js
+++ api/mt5.js
@@ -1,1 +1,14 @@
+export default async function handler(req, res) {
 
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      error: "Method Not Allowed"
+    });
+  }
+
+  return res.status(200).json({
+    status: "received",
+    message: "MT5 connected successfully"
+  });
+
+}
