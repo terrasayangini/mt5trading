c983bf6fae76171192525a738693577600deb36d
terrasayangini <ircham.ferdiansyah@gmail.com>
2026-08-07 17:52:36 +0700

    Update state.js

--- api/state.js
+++ api/state.js
@@ -1,1 +1,11 @@
-
+export default function handler(req, res) {
+  res.status(200).json({
+    status: "ok",
+    market: {
+      symbol: "EURUSD",
+      timeframe: "M30",
+      price: 1.15221,
+      time: "2026-08-07 09:35"
+    }
+  });
+}
